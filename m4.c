faf7f32f4fb3c2e8e53fdf6c38ed5c11c0659112
beerInder <Beerindersingh5602@conestogac.on.ca>
2023-12-01 23:46:21 +0530

    final code with game processing function

--- m4.c
+++ m4.c
@@ -12,11 +12,59 @@ int main() {
     char *teamFiles[] = {"Toronto Maple Leafs.txt", "New York Yankees.txt", "Manchester United.txt", "Chennai Super Kings.txt"};
     int numFiles = sizeof(teamFiles) / sizeof(teamFiles[0]);
 
+    for (i = 0; i < numFiles; i++) {
+        if (processGames(teamFiles[i]) != 0) {
+            fprintf(stderr, "Error processing file: %s\n", teamFiles[i]);
+        }
+    }
+
     return 0;
 }
 
 // processGames function
+int processGames(const char *filename) {
+    FILE *file;
+    char line[100], opponentName[50];
+    int teamScore, opponentScore;
+    int wins = 0, losses = 0, ties = 0;
+    int status = 0;
 
+    file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        line[strcspn(line, "\n")] = 0;  // Remove newline character
+
+        if (strlen(line) == 0) continue;  // Skip empty lines
+
+        if (parseLine(line, opponentName, &teamScore, &opponentScore) == 0) {
+            if (teamScore > opponentScore) {
+                wins++;
+                printf("\t%s beat %s %d-%d\n", filename, opponentName, teamScore, opponentScore);
+            } else if (teamScore < opponentScore) {
+                losses++;
+                printf("\t%s lost to %s %d-%d\n", filename, opponentName, opponentScore, teamScore);
+            } else {
+                ties++;
+                printf("\t%s and %s tied at %d\n", filename, opponentName, teamScore);
+            }
+        } else {
+            fprintf(stderr, "Error parsing line: %s\n", line);
+            status = 1;
+        }
+    }
+
+    if (wins + losses + ties > 0) {
+        float winningPercentage = (2.0 * wins + ties) / (2.0 * (wins + losses + ties));
+        printf("Season result for %s: %.3f (%d-%d-%d)\n\n", filename, winningPercentage, wins, losses, ties);
+    }
+
+    fclose(file);
+    return status;
+}
 
 // parseLine function
 int parseLine(const char *line, char *opponentName, int *teamScore, int *opponentScore) {
